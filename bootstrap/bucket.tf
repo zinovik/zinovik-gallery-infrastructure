fbed5e5da70c6a0abd764aa455148977a51bc4e1
Max Zinovik <zinovik@gmail.com>
2026-06-17 20:28:58 +0200

    add service accounts permissions to new bucket

--- bootstrap/bucket.tf
+++ bootstrap/bucket.tf
@@ -22,10 +22,28 @@ resource "google_storage_bucket" "gallery-files" {
   }
 }
 
-# Permission for old api service account, will be removed
+# Permissions for other projects
 
-resource "google_storage_bucket_iam_member" "gallery_files_viewer" {
+resource "google_storage_bucket_iam_member" "storage_tools_object_admin" {
   bucket = google_storage_bucket.gallery-files.name
-  role   = "roles/storage.legacyBucketReader"
-  member = "serviceAccount:gallery@zinovik-project.iam.gserviceaccount.com"
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:storage-tools@zinovik-project.iam.gserviceaccount.com"
+}
+
+resource "google_storage_bucket_iam_member" "fuftyfu_api_object_viewer" {
+  bucket = google_storage_bucket.gallery-files.name
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:fuftyfu@zinovik-project.iam.gserviceaccount.com"
+}
+
+resource "google_storage_bucket_iam_member" "fuftyfu_api_object_creator" {
+  bucket = google_storage_bucket.gallery-files.name
+  role   = "roles/storage.objectCreator"
+  member = "serviceAccount:fuftyfu@zinovik-project.iam.gserviceaccount.com"
+}
+
+resource "google_service_account_iam_member" "fuftyfu_api_token_creator" {
+  service_account_id = "projects/zinovik-project/serviceAccounts/fuftyfu@zinovik-project.iam.gserviceaccount.com"
+  role               = "roles/iam.serviceAccountTokenCreator"
+  member             = "serviceAccount:fuftyfu@zinovik-project.iam.gserviceaccount.com"
 }
--- bootstrap/sa_iam_bindings_services.tf
+++ bootstrap/sa_iam_bindings_services.tf
@@ -10,3 +10,30 @@ resource "google_secret_manager_secret_iam_member" "gallery_api_jwt_secret_acces
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${google_service_account.gallery_api.email}"
 }
+
+resource "google_storage_bucket_iam_member" "gallery_api_viewer" {
+  bucket = google_storage_bucket.gallery-files.name
+  role   = "roles/storage.legacyBucketReader"
+  member = "serviceAccount:${google_service_account.gallery_api.email}"
+}
+
+resource "google_storage_bucket_iam_member" "gallery_api_object_viewer" {
+  bucket = google_storage_bucket.gallery-files.name
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:${google_service_account.gallery_api.email}"
+}
+
+
+resource "google_service_account_iam_member" "gallery_api_token_creator" {
+  service_account_id = google_service_account.gallery_api.name
+  role               = "roles/iam.serviceAccountTokenCreator"
+  member             = "serviceAccount:${google_service_account.gallery_api.email}"
+}
+
+# old bucket with json files, will be removed
+
+resource "google_storage_bucket_iam_member" "gallery_api_object_admin" {
+  bucket = "zinovik-gallery"
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.gallery_api.email}"
+}
